db99870b5eaf6163ef3eea2ef9801fa2e95aaf0a
whitepony22 <gracethomas2932@gmail.com>
2025-07-26 11:33:25 +0530

    debugging

--- MsSQLinDocker/app/main.py
+++ MsSQLinDocker/app/main.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify
-from MsSQLinDocker.app.mssqlConnector import MSSQLConnector  # import your class
+from app.mssqlConnector import MSSQLConnector  # import your class
 
 app = Flask(__name__)
 password = os.environ.get("MsSQLPsswd", "")
